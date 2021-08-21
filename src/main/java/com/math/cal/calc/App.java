f94cc4854b8967adcefcf5c2034d86dafbe3c0bd
bhatti <gur6565@gmail.com>
2021-08-21 01:34:17 -0700

    calculator

---
+++ src/main/java/com/math/cal/calc/App.java
@@ -0,0 +1,13 @@
+package com.math.cal.calc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/math/cal/calc/calculator.java
@@ -0,0 +1,15 @@
+package com.math.cal.calc;
+
+public class calculator 
+{
+	public static void main(String[] args) {
+        
+	      int num1 = 5, num2 = 15, sum, mul;
+	      sum = num1 + num2;
+mul = num1 * num2;
+	      System.out.println("Sum of these numbers: "+sum);
+	      System.out.println("mul of these numbers: "+mul);
+	   }
+	}
+
+
